1fc81928f13b38da303ef4b6c22789e13e0813f0
Zhengyi Luo <zhengyiluo5@gmail.com>
2026-07-11 14:39:03 -0700

    Defer mobile notification permission

--- mobile-react/src/store/useAppStore.ts
+++ mobile-react/src/store/useAppStore.ts
@@ -160,7 +160,7 @@ export const useAppStore = create<AppState>((set, get) => ({
       if (cached) set(state => ({ snapshots: { ...state.snapshots, [selected]: cached } }))
     }
     await get().reconnect()
-    void Notifications.requestPermissionsAsync().catch(() => { /* unavailable in unsigned simulator builds */ })
+    if (get().connected) void Notifications.requestPermissionsAsync().catch(() => { /* unavailable in unsigned simulator builds */ })
     void updateBadge(get().sessions)
     if (refreshTimer) clearInterval(refreshTimer)
     refreshTimer = setInterval(() => { void get().refreshSessions() }, 12_000)
